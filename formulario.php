3c8571b607dc84c73d998d04e9cad0c63519194b
Davidtricolor2021 <92552378+Davidtricolor2021@users.noreply.github.com>
2022-05-26 08:09:45 -0300

    Create formulario.php

---
+++ formulario.php
@@ -0,0 +1,33 @@
+<form>
+    <input type="hidden" name="id" value="<?php echo $contato['id']; ?>" />
+    <fieldset>
+        <legend>Novo Contato</legend>
+            <label>
+                Nome:
+                <input type="text" name="nome" value="<?php	echo $contato['nome']; ?>" />
+            </label>
+            <label>
+                Telefone:
+                <input type="text" name="telefone" value="<?php	echo $contato['telefone']; ?>" />
+            </label>
+            <label>
+                E-mail:
+                <input type="text" name="email" value="<?php echo $contato['email']; ?>" />
+            </label>
+            <label>
+                Descrição (Opcional):
+                <textarea name="descricao">
+                    <?php echo $contato['descricao']; ?>
+                </textarea>
+            </label>
+            <label>
+                Data de nascimento:
+                <input type="text" name="nascimento" value="<?php echo traduz_data_para_exibir($contato['nascimento']); ?>"/>
+            </label>
+            <label>
+                Favorito:
+                <input type="checkbox" name="favorito" value="1" <?php echo ($contato['favorito'] == 1) ? 'checked' : ''; ?> />
+            </label>
+                <input type="submit" value="<?php echo ($contato['id'] > 0) ? 'Atualizar' : 'Cadastrar'; ?>" />
+    </fieldset>
+</form>
